aecbf90929481440d0a49ce35a134103764e4064
tnsbro <flower58570@gmail.com>
2024-12-27 11:58:16 +0900

    update img, code

--- api/index.py
+++ api/index.py
@@ -1,15 +1,28 @@
 from flask import Flask, request, render_template, redirect, url_for, make_response, send_from_directory
 from pymongo import MongoClient
+from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # 비밀 키 설정
 
 client = MongoClient("mongodb+srv://ambition:ambition@cluster0.oycph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
 db = client['ambition']
 members_collection = db['members']
 
+serializer = URLSafeTimedSerializer(app.secret_key)
+
 @app.route('/')
 def index():
-    return render_template('main.html')
+    email_cookie = request.cookies.get('email')
+    if email_cookie:
+        try:
+            email = serializer.loads(email_cookie, max_age=60*60*24*30)  # 쿠키 유효기간 30일
+            member = members_collection.find_one({'email': email})
+            if member:
+                return render_template('mypage.html', member=member)
+        except:
+            return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/find')
 def find_choice():
@@ -25,11 +38,17 @@ def stampimg():
 
 @app.route('/mypage')
 def mypage():
-    member = members_collection.find_one({'email': request.cookies.get('email')})
-    if member:
-        if 'stamp' not in member:
-            member['stamp'] = 0
-        return render_template('mypage.html', member=member)
+    email_cookie = request.cookies.get('email')
+    if email_cookie:
+        try:
+            email = serializer.loads(email_cookie, max_age=60*60*24*30)  # 쿠키 유효기간 30일
+            member = members_collection.find_one({'email': email})
+            if member:
+                if 'stamp' not in member:
+                    member['stamp'] = 0
+                return render_template('mypage.html', member=member)
+        except:
+            return redirect(url_for('login'))
     return redirect(url_for('login'))
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -37,8 +56,9 @@ def login():
     if request.method == 'POST':
         member = members_collection.find_one({'email': request.form['email']})
         if member and member['password'] == request.form['password']:
-            response = make_response(redirect(url_for('index')))
-            response.set_cookie('email', request.form['email'], max_age=60*60*24*30)  # 쿠키 유효기간 30일
+            response = make_response(redirect(url_for('mypage')))
+            email_cookie = serializer.dumps(request.form['email'])
+            response.set_cookie('email', email_cookie, max_age=60*60*24*30)  # 쿠키 유효기간 30일
             return response
         return render_template('login.html', error=401)
     return render_template('login.html')
@@ -64,7 +84,6 @@ def logout():
     response = make_response(redirect(url_for('index')))
     response.delete_cookie('email')
     return response
-
 
 if __name__ == '__main__':
     app.run(debug=True)
